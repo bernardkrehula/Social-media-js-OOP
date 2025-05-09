4373dc6982ef71d09eda528063c2f0cefb150e5c
bernardkrehula <krehula.bernard456@gmail.com>
2025-05-09 22:24:00 +0200

    Display friends

---
+++ friendsList.js
@@ -0,0 +1,38 @@
+export const friends = [
+    {                  
+      id: crypto.randomUUID(),
+      firstName: "Gordana",
+      lastName: "Stouns",
+      img: "/images/profile1.jpg",
+    },
+    {
+      id: crypto.randomUUID(),
+      firstName: "Hiroshi",
+      lastName: "Tanaka",
+      img: "/images/profile2.jpg",
+    },
+    {
+      id: crypto.randomUUID(),
+      firstName: "Kilibarda",
+      lastName: "Petrovska",
+      img: "/images/profile3.jpg",
+    },
+    {
+      id: crypto.randomUUID(), 
+      firstName: "Majda",
+      lastName: "Odzaklijevska",
+      img: "/images/profile4.jpg",
+    },
+    {
+      id: crypto.randomUUID(),
+      firstName: "Ethan",
+      lastName: "Turner",
+      img: "/images/profile5.jpg",
+    },
+    {
+      id: crypto.randomUUID(),
+      firstName: "Marc",
+      lastName: "Anderson",
+      img: "/images/profile6.jpg",
+    },
+]
--- script.js
+++ script.js
@@ -5,3 +5,20 @@
 //Koji su mi objekti (instance klasa) potrebni?
 //U kojim arrayevima treba da budu ti objekti tj ko upravlja tim objektima
 //Gdje treba da se nalaze ovi arrayevi?
+import { friends } from "./friendsList.js";
+
+const friendsList = document.querySelector('.friends');
+const friendListImages = document.querySelector('.friend-images');
+
+friends.forEach(friend => displayFriends(friend));
+
+function displayFriends(friend){
+    let html = `
+    <li>
+        <img src="${friend.img}">
+        <h3>${friend.firstName} ${friend.lastName}</h3>
+    </li>
+    `
+
+    friendListImages.insertAdjacentHTML('beforeend', html);
+}
