0f50f38f50244b631309f764712df79b0c4a0810
Bernard Krehula <krehula.bernard456@gmail.com>
2025-06-10 18:25:32 +0200

    Request pokemon on search

--- socialMediaManager.js
+++ socialMediaManager.js
@@ -65,7 +65,7 @@ export class Post {
         const foundComment = this.findComment(id);
 
         if(foundComment.isUserComment){
-            const comment = comments.querySelector(`#${id}`);
+            const comment = comments.querySelector(`.comment[id="${id}"]`);
             comment.innerHTML = ` 
             <img src="${foundComment.userImg}">
             <div class="comment-name">
@@ -85,7 +85,7 @@ export class Post {
     //Vidijeti kako napraviti apstrakciju za innerHtml
     //Moguce editovat samo sobstveni komentar (modal se zatvara klikom van njega)
     saveEditedComment(id, comments){
-        const comment = comments.querySelector(`#${id}`);
+        const comment = comments.querySelector(`.comment[id="${id}"]`);
         const foundComment = this.findComment(id);
         comment.innerHTML = ` 
             <img src="${foundComment.userImg}">
@@ -98,7 +98,7 @@ export class Post {
                 <button class="editComment"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg> Edit</button>
                 <button class="deleteComment"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg> Delete</button>
             </div>
-        `;
+        `; 
     }
     //Selektirati preko posta 
     removeComment(id, comments){
